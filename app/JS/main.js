9c407a48e92dfdc2d12209e062431ec59670bbf7
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-11-19 21:35:01 -0500

    eaeaea

--- app/JS/main.js
+++ app/JS/main.js
@@ -5,39 +5,35 @@ import "../CSS/style.css";
 import { DOMSelectors } from "./DOMSelectors.js";
 async function getData() {
   //"tries" code and if it does not achieve a certain output, it returns an error
-  const cardarray = []; //all the cards
+  let cardarray = []; //all the cards
   let page = 1;
   try {
-    for (i = page; page > 0; page++) {
-      //FIX THIS LATER im lazy
-      //returns a promise
-      const response = await fetch("https://api.pokemontcg.io/v2/cards");
-      //guard clause
-      console.log(response.status);
-      if (response.status != 200) {
-        throw new Error("Cant find data");
-      } else {
-        //convert promise to json
-        const carddata = await response.json();
-        //adds cards to main array
-        carddata.data.forEach((card) => {
-          cardarray.push(card);
-        });
-      }
-      //make smth for checking when cards reach max it breaks
+    // for (i = page; page < maxpage#; page++) { FIX THIS LATER im lazy
+    //returns a promise
+    const response = await fetch("https://api.pokemontcg.io/v2/cards");
+    //guard clause
+    console.log(response.status);
+    console.log("e");
+    if (response.status != 200) {
+      throw new Error("Cant find data");
+    } else {
+      //convert promise to json
+      const carddata = await response.json();
+      //adds cards to main array
+      carddata.data.forEach((card) => {
+        cardarray.push(card);
+      });
     }
+    //make smth for checking when cards reach max it breaks
   } catch (error) {
+    // }
     alert("couldnt find that card");
   }
-  return cardarray;
-}
-getData();
-function cardCreator(x) {
-  x.forEach((card) =>
+  cardarray.forEach((card) =>
     DOMSelectors.container.insertAdjacentHTML(
       "afterbegin",
       `<h1>${card.name}</h1>`
     )
   );
 }
-cardCreator(getData());
+getData();
