40b6ff93ff847b6016149993bda57e83311535c6
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-12-02 12:32:49 -0500

    lidfjdslifasl

--- app/JS/DOMSelectors.js
+++ app/JS/DOMSelectors.js
@@ -1,7 +1,7 @@
 const DOMSelectors = {
   body: document.querySelector(".body"),
   container: document.querySelector("#cardtainer"),
-  optioncontainer: document.querySelector(".optioncontainer"),
+  optioncontainer: document.querySelector("#optioncontainer"),
   maindropdown: document.querySelector("#typedropdown"),
 };
 export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -3,14 +3,14 @@ import "../CSS/style.css";
 //promises
 //show data
 import { DOMSelectors } from "./DOMSelectors.js";
-let setarray = [];
 let dropdown = [];
-async function createDropdowns() {
+async function createSetDropdowns() {
+  let setarray = [];
   DOMSelectors.optioncontainer.innerHTML = "";
   DOMSelectors.container.innerHTML = "";
   DOMSelectors.optioncontainer.insertAdjacentHTML(
     "afterbegin",
-    `<select class="select select-bordered w-full max-w-xs" id="setdropdown">
+    `<select class="select select-bordered w-full max-w-xs bg-purple text-TextColor" id="setdropdown">
       <option value="unselectable" disabled selected>Pick a set</option>
     </select>`
   );
@@ -38,8 +38,43 @@ async function createDropdowns() {
     );
   });
 }
+async function createRarityDropdowns() {
+  let rarityarray = [];
+  dropdown = document.querySelector("#setdropdown");
+  DOMSelectors.optioncontainer.insertAdjacentHTML(
+    "afterbegin",
+    `<input type="text" placeholder="Search..." class="py-2 px-4  rounded mb-4" id="Set Search">
+    <details class="dropdown relative text-TextColor" id="raritydropdown">
+      <summary class="btn m-1">Look for Rarities</summary>
+      <ul class="menu dropdown-content bg-TopBar rounded-box z-10 w-52 p-2 absolute">
+      </ul>
+    </details>`
+  );
+  dropdown = document.querySelector("#raritydropdown");
+  try {
+    //returns a promise
+    const response = await fetch("https://api.pokemontcg.io/v2/rarities");
+    //guard clause
+    if (response.status != 200) {
+      throw new Error("Cant find data");
+    } else {
+      const raritydata = await response.json();
+      raritydata.data.forEach((rarity) => {
+        rarityarray.push(rarity);
+      });
+    }
+  } catch (error) {
+    alert("couldnt fetch api stop frickin spamming refresh");
+  }
+  rarityarray.forEach((rarity) => {
+    dropdown.insertAdjacentHTML(
+      "beforeend",
+      `<li class="bg-purple"<a>${rarity}</a></li>` //creates all dropdown functions
+    );
+  });
+}
 async function createSetData() {
-  await createDropdowns();
+  await createSetDropdowns();
   const response = await fetch("https://api.pokemontcg.io/v2/sets/");
   const carddata = await response.json();
   dropdown.addEventListener("change", function () {
@@ -55,11 +90,11 @@ async function createSetData() {
       //for going through cuz some sets are missing the attribute
       DOMSelectors.container.insertAdjacentHTML(
         "afterbegin",
-        `<div class="card bg-CardBG w-96 shadow-xl">
+        `<div class="card bg-CardBG w-110 shadow-xl text-TextColor">
         <figure>
           <img
             src="${selectedcard[0].images.logo}"
-            alt="setimage" />
+            alt="${selectedcard[0].name} Official Set Logo from Pokemon" />
         </figure>
         <div class="card-body">
           <h2 class="card-title">
@@ -110,9 +145,13 @@ async function createSetData() {
 async function createRarities() {
   //"tries" code and if it does not achieve a certain output, it returns an error
   let raritycard = []; //card examples picked later
+  DOMSelectors.optioncontainer.innerHTML = "";
+  DOMSelectors.container.innerHTML = "";
+  await createRarityDropdowns();
   try {
     //returns a promise
     const response = await fetch(`https://api.pokemontcg.io/v2/cards`);
+    const rarities = await fetch("https://api.pokemontcg.io/v2/rarities");
     //guard clause
     if (response.status != 200) {
       throw new Error("Cant find data");
@@ -140,7 +179,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (DOMSelectors.maindropdown.value == "sets") {
       createSetData(); //if set itll set up for sets
     } else {
-      console.log("filler"); //if not itll set up for rarities
+      createRarities(); //if not itll set up for rarities
     }
   });
 });
